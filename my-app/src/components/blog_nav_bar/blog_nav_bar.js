9435c8fb6e891a956522d7462d64bba9ddd1ef41
Krista Mätäsniemi <krista.matasniemi@tuni.fi>
2023-07-21 18:15:11 +0300

    Try to add pictures

--- my-app/src/components/blog_nav_bar/blog_nav_bar.js
+++ my-app/src/components/blog_nav_bar/blog_nav_bar.js
@@ -1,10 +1,14 @@
-import * as React from 'react';
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 import './blog_nav_bar.css'
 
 
 export default function BlogNavbar() {
+
+    const businessBtn = document.getElementById("business-button");
+    const technologyBtn = document.getElementById("technology-button");
+    const uxBtn = document.getElementById("ux-button");
 
     const navigate = useNavigate()
 
@@ -13,26 +17,13 @@ export default function BlogNavbar() {
       navigate(page);
     };
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-
-    // Open the menu bar
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-
-    // Close the menu bar
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-
     return (
       <nav className="blog-navigation">
 
         <div>
-          <button className={'blog-nav_button red-theme'} onClick={() => navigateToPage('/blog/business')}>Business</button>
-          <button className={'blog-nav_button blue-theme'} onClick={() => navigateToPage('/blog/technology')}>Technology</button>
-          <button className={'blog-nav_button yellow-theme'} onClick={() => navigateToPage('/blog/ux')}>UX</button>
+          <button id="business-button" className={'blog-nav_button red-theme'} onClick={() => navigateToPage('/blog/business')}>Business</button>
+          <button id="technology-button" className={'blog-nav_button blue-theme'} onClick={() => navigateToPage('/blog/technology')}>Technology</button>
+          <button id="ux-button" className={'blog-nav_button yellow-theme'} onClick={() => navigateToPage('/blog/ux')}>UX</button>
         </div>
 
       </nav>
