43e81381c4884dcc7355788e3794d130e7af48d5
Krista Mätäsniemi <krista.matasniemi@tuni.fi>
2023-08-03 18:53:04 +0300

    Fix textbox positions in comparison diagram

--- my-app/src/components/navigation/comparison.js
+++ my-app/src/components/navigation/comparison.js
@@ -25,16 +25,22 @@ export default function ComparisonDiagram ({variant = "comp2"|"comp3", titles = 
                 return (
                     <div className="container higher">
                         <div className="circle first">
-                            <Typography>{titles[0]}</Typography>
-                            <Typography>{contents[0]}</Typography>
+                            <div className="textbox">
+                                <Typography>{titles[0]}</Typography>
+                                <Typography>{contents[0]}</Typography>
+                            </div>
                         </div>
                         <div className="circle second">
-                            <Typography>{titles[1]}</Typography>
-                            <Typography>{contents[1]}</Typography>
+                            <div className="textbox">
+                                <Typography>{titles[1]}</Typography>
+                                <Typography>{contents[1]}</Typography>
+                            </div>
                         </div>
                         <div className="circle third">
-                            <Typography>{titles[1]}</Typography>
-                            <Typography>{contents[1]}</Typography>
+                            <div className="textbox">
+                                <Typography>{titles[1]}</Typography>
+                                <Typography>{contents[1]}</Typography>
+                            </div>
                         </div>
                     </div>
                 );
