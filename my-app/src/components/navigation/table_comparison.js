8ff6609d0be1a3ff7ca02ef7c3fa39fac6df34bd
Julia <julia.harttunen@tuni.fi>
2023-08-11 18:53:36 +0300

    Create a link header inside the comparison table

--- my-app/src/components/navigation/table_comparison.js
+++ my-app/src/components/navigation/table_comparison.js
@@ -1,23 +1,11 @@
 import './table_comparison.css';
+import {Link} from 'react-router-dom';
 
 // Example of a data array that we might receive from an API
 const test_data = [
-    { header: "header1", feature1: 'column 1.1', feature2: "column 1.2" },
-    { header: "header2", feature1: 'column 2.1', feature2: "column 2.2" },
-    { header: "header1", feature1: 'column 1.1', feature2: "column 1.2" },
-    { header: "header2", feature1: 'column 2.1', feature2: "column 2.2" },
-    { header: "header1", feature1: 'column 1.1', feature2: "column 1.2" },
-    { header: "header2", feature1: 'column 2.1', feature2: "column 2.2" },
-    { header: "header1", feature1: 'column 1.1', feature2: "column 1.2" },
-    { header: "header2", feature1: 'column 2.1', feature2: "column 2.2" },
-    { header: "header1", feature1: 'column 1.1', feature2: "column 1.2" },
-    { header: "header2", feature1: 'column 2.1', feature2: "column 2.2" },
-    { header: "header1", feature1: 'column 1.1', feature2: "column 1.2" },
-    { header: "header2", feature1: 'column 2.1', feature2: "column 2.2" },
-    { header: "header1", feature1: 'column 1.1', feature2: "column 1.2" },
-    { header: "header2", feature1: 'column 2.1', feature2: "column 2.2" },
-    { header: "header1", feature1: 'column 1.1', feature2: "column 1.2" },
-    { header: "header2", feature1: 'column 2.1', feature2: "column 2.2" },
+    { navigation: '/feedback', header: "header1", feature1: 'column 1.1', feature2: "column 1.2" },
+    { navigation: 'https://medium.com/@bfgmartin/what-is-a-product-manager-ce0efdcf114c', header: "header2", feature1: 'column 2.1', feature2: "column 2.2" },
+    { header: "header1", feature1: 'column 3.1', feature2: "column 3.2" },
 ]
 
 function Table() {
@@ -28,7 +16,7 @@ function Table() {
 
     //console.log(Object.keys(data[0]).length); //3
 
-    //console.log(Object.keys(data[0])[0]); //header
+    //console.log(Object.keys(test_data[0])[0]); //header
 
     const getTableHeaders = () => {
 
@@ -38,32 +26,56 @@ function Table() {
         return (
           <tr>
             {
-              features.map((item) => (
-                <th className='th-row'>{item}</th>
-              ))
+              features.map((item) => {
+
+                if ( item !== 'navigation' ) {
+                  return <th className='th-row'>{item}</th>
+                }
+
+              })
             }
           </tr>
         )
     }
 
     const getTableBody = () => {
+
+      // Link of field
+      let navigationPage = '';
     
         return (!test_data) ? null : (
           <tbody>
             {test_data.map((item) => {
 
-              //console.log('item: ', item);
-
               return (
                 <tr>
                   {Object.entries(item).map((field) => {
 
-                    //console.log('field: ', field);
-
-                    if ( field[0] === 'header' ) {
-                      return <th className='th-column'>{field[1]}</th>
+                    if ( field[0] === 'navigation' ) {
+                      navigationPage = field[1];
                     }
+
+                    else if ( field[0] === 'header' ) {
+
+                      // Let's check if the header is a link
+                      if ( navigationPage !== '' ) {
+
+                        return <th className='th-column'>
+                            <Link 
+                              className='th-link'
+                              to={navigationPage}
+                              >{field[1]}</Link>
+                          </th>
+
+                      }
+                      else {
+                        return <th className='th-column'>{field[1]}</th>
+                      }
+
+                    }
+
                     else {
+                      navigationPage = '';
                       return <td>{field[1]}</td> // 0 -> keys, 1 -> values
                     }
                     
@@ -88,55 +100,3 @@ function Table() {
 }
 
 export default Table;
-
-/*
-	{ row1: "row1", row2: 'row2', row3: "row3" },
-	{ row1: "row4", row2: 'row5', row3: "row6" },
-*/
-/*
-    ['ööjotain', 'feature1', 'feature2'],
-    ['Header 1', 'column 1', 'column 2'],
-    ['Header 2', 'column 1', 'column 2'],
-*/
-/*
-    { header: "header", feature1: 'column 1', feature2: "column 2" },
-    { header: "header", feature1: 'column 1', feature2: "column 2" },
-*/
-
-/*
-	return (
-		<div className="table-section">
-			<table>
-				<tr>
-                    {
-                        data[0].map((feature, index) => (
-                            <th>{feature}</th>
-                        ))
-                    }
-				</tr>
-				{data.map((val, key) => {
-					return (
-						<tr key={key}>
-							<td>{val.secondkey}</td>
-						</tr>
-					)
-				})}
-			</table>
-		</div>
-	);
-*/
-
-/*
-              return (
-                <tr>
-                  {Object.entries(item).map((field) => {
-
-                    console.log('field: ', field);
-
-                    return <th>{field[0]}</th> // 0 -> keys, 1 -> values
-                  })}
-                </tr>
-              );
-*/
-
-//{getTableBodyAsReactElement()}
