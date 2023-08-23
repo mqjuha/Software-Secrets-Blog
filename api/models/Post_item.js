3e89fdc6ced1780e37e95bc1b8b1b0cf6a3a9416
Krista Mätäsniemi <krista.matasniemi@tuni.fi>
2023-08-23 18:10:42 +0300

    Create schema for articles

---
+++ api/models/Post_item.js
@@ -0,0 +1,23 @@
+_id, title, cover, abstract, nav, content, summary, references
+
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const PostSchema = new Schema({
+    title: String,
+    cover: String,
+    abstract: String,
+    nav: {
+        element: String,
+        content: String
+    },
+    content: String,
+    summary: String,
+    references: [{
+        ref: String
+    }]
+});
+
+const PostModel =  model('Post', PostSchema);
+
+module.exports = PostModel;
