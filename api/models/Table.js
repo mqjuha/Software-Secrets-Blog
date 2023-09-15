de5948b03af1fa0a7d00291606fb99a702883ee8
Julia <julia.harttunen@tuni.fi>
2023-09-15 18:16:32 +0300

    Create a schema for table comparison

---
+++ api/models/Table.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const TableSchema = new Schema({
+    title: {type: String, require: true},
+    table: [{
+        header_row: [{type: String, require: true}],
+        content: [{
+            navigation: {type: String, require: false},
+            items: [{type: String, require: true}],
+        }   
+        ]
+    }
+    ]  
+}, { collection: 'table'});
+
+const Table =  model('Table', TableSchema);
+
+module.exports = Table;
