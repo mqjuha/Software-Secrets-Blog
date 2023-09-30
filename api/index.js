0e556002fe07940bdc90594e4be53fadfa19b2f0
Julia <julia.harttunen@tuni.fi>
2023-09-30 11:14:12 +0300

    Try to tun server and front end at the same time

--- api/index.js
+++ api/index.js
@@ -37,6 +37,95 @@ app.get('/aboutus', async (req,res) => {
     );
 });
 
+app.get('/blog', async (req,res) => {
+    res.json(
+      await Post.find()
+    );
+});
+
+app.get('/blog/:id', async (req, res) => {
+
+    console.log("GET: Article")
+    const {id} = req.params;
+
+    res.json(
+        await Post.findById(id)
+    );
+
+});
+
+app.get('/timelineHor/:id', async (req, res) => {
+    console.log("GET: TimelineHor")
+    const {id} = req.params;
+
+    res.json(
+        await TimelineHor.findById(id)
+    );
+});
+
+app.get('/timelineVer/:id', async (req, res) => {
+    console.log("GET: TimelineVer")
+    const {id} = req.params;
+
+    res.json(
+        await TimelineVer.findById(id)
+    );
+});
+
+app.get('/tableComponent/:id', async (req,res) => {
+    console.log('GET: Table');
+    const {id} = req.params;
+    res.json(
+      await TableComponent.findById(id)
+    );
+});
+
+app.get('/reference/:id', async (req,res) => {
+    console.log('GET: Reference');
+    const {id} = req.params;
+    res.json(
+      await Ref.findById(id)
+    );
+});
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
+
+/*    const person = 
+        { 
+            name: 'First Last',
+            job: 'Student',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nibh purus, porta sit amet ultrices in, aliquet sed libero. Nulla a accumsan turpis. Cras aliquet, nunc sollicitudin bibendum iaculis, arcu ante pellentesque nisl, nec tristique nibh quam eu lorem. Vivamus vel quam a neque molestie pulvinar. Sed nec euismod est. Vestibulum at luctus tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ullamcorper lacus a tortor iaculis fringilla.',
+            img: "https://cdn.pixabay.com/photo/2015/04/17/03/01/the-locale-726512_1280.jpg",
+            linkedin: '',
+            email: 'first.last@gmail.com',
+    };
+    
+    itemsCollection.insertOne(person)
+        .then(result => console.log(`Successfully inserted item with _id: ${result.insertedId}`))
+        .catch(err => console.error(`Failed to insert item: ${err}`))
+*/
+
+/*
+app.post('/', async (req,res) => {
+    const {testname, content} = req.body;
+    const referenceDoc =  await Reference.create({
+        author: "author",
+        title: "content"
+    })
+    res.json(referenceDoc);
+});
+
+
+
+app.get('/post/:id', async (req, res) => {
+    const {id} = req.params;
+    const postDoc = await Post.findById(id);
+    res.json(postDoc);
+});
+*/
+
 /*
 app.get('/blog', async (req,res) => {
     console.log('GET: TimelineVer');
@@ -113,101 +202,4 @@ app.post('/', async (req,res) => {
     })
     res.json(postDoc);
 });
-*/
-
-app.get('/blog', async (req,res) => {
-    res.json(
-      await Post.find()
-    );
-});
-
-app.get('/blog/:id', async (req, res) => {
-
-    console.log("GET: Article")
-    const {id} = req.params;
-
-    res.json(
-        await Post.findById(id)
-    );
-    
-    // .populate(nav.element)
-/*
-    TimelineHor.findById(nav.id)
-    .then(timelineHorFound => {
-        if (timelineHorFound) { return res.json(timelineHorFound); }
-    })*/
-
-});
-
-app.get('/timelineHor/:id', async (req, res) => {
-    console.log("GET: TimelineHor")
-    const {id} = req.params;
-
-    res.json(
-        await TimelineHor.findById(id)
-    );
-});
-
-app.get('/timelineVer/:id', async (req, res) => {
-    console.log("GET: TimelineVer")
-    const {id} = req.params;
-
-    res.json(
-        await TimelineVer.findById(id)
-    );
-});
-
-app.get('/tableComponent/:id', async (req,res) => {
-    console.log('GET: Table');
-    const {id} = req.params;
-    res.json(
-      await TableComponent.findById(id)
-    );
-});
-
-app.get('/reference/:id', async (req,res) => {
-    console.log('GET: Reference');
-    const {id} = req.params;
-    res.json(
-      await Ref.findById(id)
-    );
-});
-
-
-/*
-app.post('/', async (req,res) => {
-    const {testname, content} = req.body;
-    const referenceDoc =  await Reference.create({
-        author: "author",
-        title: "content"
-    })
-    res.json(referenceDoc);
-});
-
-
-
-app.get('/post/:id', async (req, res) => {
-    const {id} = req.params;
-    const postDoc = await Post.findById(id);
-    res.json(postDoc);
-});
-*/
-
-app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
-});
-
-/*    const person = 
-        { 
-            name: 'First Last',
-            job: 'Student',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nibh purus, porta sit amet ultrices in, aliquet sed libero. Nulla a accumsan turpis. Cras aliquet, nunc sollicitudin bibendum iaculis, arcu ante pellentesque nisl, nec tristique nibh quam eu lorem. Vivamus vel quam a neque molestie pulvinar. Sed nec euismod est. Vestibulum at luctus tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ullamcorper lacus a tortor iaculis fringilla.',
-            img: "https://cdn.pixabay.com/photo/2015/04/17/03/01/the-locale-726512_1280.jpg",
-            linkedin: '',
-            email: 'first.last@gmail.com',
-    };
-    
-    itemsCollection.insertOne(person)
-        .then(result => console.log(`Successfully inserted item with _id: ${result.insertedId}`))
-        .catch(err => console.error(`Failed to insert item: ${err}`))
 */
